57459202c04f79de44eb3b0cfa33a39e69af3cbd
Danil <d.kabirov@innopolis.ru>
2020-05-25 22:00:47 +0300

    init

---
+++ WinServ2501.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+
+#include "pch.h"
+#include "server.h"
+#include "client.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2 || (string)(argv[1]) == "server") {
+		cout << "Sever mode on\n";
+		return server();
+	}
+	else if ((string)(argv[1]) == "--client") {
+		cout << "Client mode on\n";
+		return client();
+	}
+	else if ((string)(argv[1]) == "service") {
+		cout << "Service mode on\n";
+	}
+}
---
+++ client.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <windows.h> 
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+
+
+int client(int argc = 0, TCHAR* argv[] = nullptr);
